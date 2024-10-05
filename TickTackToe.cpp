4ce53709adb8c82553d02bf4b1381144903a9bbd
Kermid <jakub.gibowski312@gmail.com>
2024-10-05 13:47:07 +0200

    rest of the game

--- TickTackToe.cpp
+++ TickTackToe.cpp
@@ -26,7 +26,13 @@ int main()
          {
 
             running = false;
+            break;
             
+         }
+         else if(checkTie(spaces))
+         {
+            running = true;
+            break;
          }
          computerMove(spaces,computer);
          drawBoard(spaces);
@@ -34,16 +40,23 @@ int main()
          {
 
             running = false;
+            break;
             
          }
+         else if(checkTie(spaces))
+        {
+            running = false;
+            break;
+        }
     }
+
 
     return 0;
 }
 void drawBoard(char *spaces)
 {
     std::cout <<  "     |      |     " <<  "   Oznaczenia pol:" << std::endl;
-    std::cout <<  "  " << spaces[0] << "  |  "<< spaces[1] <<"   |  "  << spaces[2] << "   " << "   1   2   3" << std::endl;
+    std::cout <<  "  " << spaces[0] << "  |  "<< spaces[1] <<"   |   "  << spaces[2] << "   " << "   1   2   3" << std::endl;
     std::cout <<  "_____|______|_____" << std::endl;
     std::cout <<  "     |      |     " << std::endl;
     std::cout <<  "  " << spaces[3] << "  |   "<< spaces[4] <<"  |   " << spaces[5] << "   " << "  4   5   6" << std::endl;
@@ -58,7 +71,7 @@ void playerMove(char *spaces,char player)
     std::cout << "Podaj number 1-9 miejsca gdzie chcesz postawic krzyzyk" << std::endl;
     std::cin >> move;
     move--;
-    while(move < 0 || move > 9)
+    while(move < 0 || move > 9 || spaces[move] != ' ')
     {
         std::cout << "Nieprawidlowy wybor.Wybierz liczbe z zakresu 1-9." << std::endl;
         std::cin >> move;
@@ -91,54 +104,63 @@ void computerMove(char *spaces,char computer)
 bool checkWinner(char *spaces,char computer,char player)
 {
     //sprawdzanie rzedow
-    if( spaces[0] == ' ' && spaces[0] == spaces[1] && spaces[1] == spaces[2])
+    if( spaces[0] != ' ' && spaces[0] == spaces[1] && spaces[1] == spaces[2])
     {
-        spaces[0] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[0] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
-    else if(spaces[3] == ' ' && spaces[3] == spaces[4] && spaces[4] == spaces[5])
+    else if(spaces[3] != ' ' && spaces[3] == spaces[4] && spaces[4] == spaces[5])
     {
-        spaces[3] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[3] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
-    else if( spaces[6] == ' ' && spaces[6] == spaces[7] && spaces[7] == spaces[8])
+    else if( spaces[6] != ' ' && spaces[6] == spaces[7] && spaces[7] == spaces[8])
     {
-        spaces[6] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[6] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
     //sprawdzanie kolumn
-     if( spaces[0] == ' ' && spaces[0] == spaces[3] && spaces[3] == spaces[6])
+     else if( spaces[0] != ' ' && spaces[0] == spaces[3] && spaces[3] == spaces[6])
     {
-        spaces[0] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[0] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
-    else if( spaces[1] == ' ' && spaces[1] == spaces[4] && spaces[4] == spaces[7])
+    else if( spaces[1] != ' ' && spaces[1] == spaces[4] && spaces[4] == spaces[7])
     {
-        spaces[1] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[1] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
-    else if(spaces[2] == ' ' && spaces[2] == spaces[5] && spaces[5] == spaces[8])
+    else if(spaces[2] != ' ' && spaces[2] == spaces[5] && spaces[5] == spaces[8])
     {
-        spaces[2] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[2] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
-
     //sprawdzanie przekatnych
-     if(spaces[0] == ' ' && spaces[0] == spaces[4] && spaces[4] == spaces[8])
+     else if (spaces[0] != ' ' && spaces[0] == spaces[4] && spaces[4] == spaces[8])
     {
-        spaces[0] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[0] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
-    else if(spaces[2] == ' ' && spaces[2] == spaces[4] && spaces[4] == spaces[6])
+    else if(spaces[2] != ' ' && spaces[2] == spaces[4] && spaces[4] == spaces[6])
     {
-        spaces[2] == player ? std:cout << "Wygrales!" << std::endl; : std:cout << "Przegrales!" << std::endl;
+        spaces[2] == player ? std::cout << "Wygrales!" << std::endl : std::cout << "Przegrales!" << std::endl;
         return true;
     }
-
-    return false;
+    else
+    {
+        return false;
+    }
+    
 }
 bool checkTie(char *spaces)
 {
-
+    for(int i = 0; i < 9 ; i++)
+    {
+        if(spaces[i] == ' ')
+        {
+            return false;
+        }
+    }
+    std::cout << "Remis!" << std::endl;
     return true;
 }
